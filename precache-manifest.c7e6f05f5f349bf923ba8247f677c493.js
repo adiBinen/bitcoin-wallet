dad18528ae0eeb3783f677ef77579b21f0125093
Adi Binenbaum <adi.binen@gmail.com>
2019-04-10 19:19:11 +0300

    Updates

---
+++ precache-manifest.c7e6f05f5f349bf923ba8247f677c493.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "027d9ebc5f5e5835586c",
+    "url": "/bitcoin-wallet/static/js/runtime~main.027d9ebc.js"
+  },
+  {
+    "revision": "3ae36392605ab85397c1",
+    "url": "/bitcoin-wallet/static/js/main.3ae36392.chunk.js"
+  },
+  {
+    "revision": "3f42d2914aa3df616232",
+    "url": "/bitcoin-wallet/static/js/1.3f42d291.chunk.js"
+  },
+  {
+    "revision": "3ae36392605ab85397c1",
+    "url": "/bitcoin-wallet/static/css/main.251da3af.chunk.css"
+  },
+  {
+    "revision": "08ed7a59ff64282e03467657fc51b6f8",
+    "url": "/bitcoin-wallet/index.html"
+  }
+];
